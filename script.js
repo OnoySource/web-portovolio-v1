1afddf257dfc061929918f90dfb3d33d5c1c2df2
OnoySource <onoysource@gmail.com>
2025-04-21 17:32:56 +0700

    update now

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-function
+const hamburger = document.getElementById('hamburger');
+
+const navLinks = document.getElementById('navLinks');
+      
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.textContent = navLinks.classList.contains('active') ? '✕' : '☰';
+});
